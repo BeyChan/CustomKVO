09f969e7e51e520f6ac64d040b745b8b9f48e809
Melody_Chan <melody@chan.com>
2016-10-29 17:57:49 +0800

    创建工程

---
+++ CustomKVO/CustomKVO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomKVO
+//
+//  Created by Melody Chan on 16/10/29.
+//  Copyright © 2016年 canlife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomKVO/CustomKVO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomKVO
+//
+//  Created by Melody Chan on 16/10/29.
+//  Copyright © 2016年 canlife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
